e60f38b889fd74642b025a745af08d4854771549
Tennyson Holloway <tennyson.holloway@gmail.com>
2016-03-08 15:36:05 -0500

    working commit

--- post-comment-with-casperjs.js
+++ post-comment-with-casperjs.js
@@ -1,4 +1,25 @@
-var fccURL = 'http://apps.fcc.gov/ecfs/upload/begin?procName=16-41&filedFrom=X';
+function stateToID(state) {
+        var states = ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NH", "NJ", "NM", "NV", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY", "AS", "FM", "MH", "MP", "PW"];
+    return states.indexOf(state.toUpperCase()) + 1;
+}
+
+var docket = '16-41';
+var userData = {
+    'name' : 'Tennyson Holloway',
+    'email' : 'tennyson.holloway@gmail.com',
+    'address.line1' : '1499 Massachusetts Ave NW',
+    'address.line2' : 'APT 114A',
+    'address.city' : 'Washington',
+    'address.state' : 'DC',
+    'address.zip' : '20005',
+    'address.plusFour' : '',
+    'comment' : 'Diverse video!'
+};
+
+
+
+var fccURL = 'http://apps.fcc.gov/ecfs/upload/begin?procName=' + docket + '&filedFrom=X';
+
 var casper = require('casper').create();
 
 casper.start(fccURL, function() {
@@ -9,17 +30,18 @@ casper.then (function(){
   this.echo('filling it out with example data');
 
   this.fill('form#process', {
-    'procName':'555',
-    'applicant': 'example',
-    'email': 'example@example.com',
-    'address.line1': '14 example st',
-    'address.line2': 'Apt #2',
-    'address.city': 'Beverly Hills',
-    'address.zip': '90210',
-    'address.plusFour': '',
-    //'address.state.id': 'example',
-    'briefComment': 'This is my test comment'
+    'procName': docket,
+    'applicant': userData['name'],
+    'email': userData['email'],
+    'address.line1': userData['address.line1'],
+    'address.line2': userData['address.line2'],
+    'address.city': userData['address.city'],
+    'address.zip': userData['address.zip'],
+    'address.plusFour': userData['address.plusFour'],
+    'address.state.id': stateToID(userData['address.state']),
+    'briefComment': userData['comment']
   }, false);
+
   this.echo('filled out, not submitted');
 });
 
@@ -35,3 +57,11 @@ casper.then(function(){
 });
 
 casper.run();
+
+/* css selector for confirm link #uploadReviewActions > li:nth-child(2) > a:nth-child(1) */
+
+/* confirmation text after hitting the confirm link: ECFS Filing Receipt - Confirmation number: 201638708720 */
+
+/* css selector for confirmation number .fieldset > h2:nth-child(1) */
+
+/* comment confirmation filing link: http://apps.fcc.gov/ecfs/comment/confirm?confirmation=201638708720 */
